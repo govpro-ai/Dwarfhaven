1be30bb8dab5c32c94932680dd2ec4a719ed14f4
❤️ <shah@priansh.com>
2023-11-12 17:14:30 -0500

    Update number.ts

--- src/number.ts
+++ src/number.ts
@@ -15,9 +15,9 @@ Number.prototype.secondsToTimestring = function () {
 
 Number.prototype.toFilesize = function () {
   const byte = 1
-  const kilobyte = byte * 1000
-  const megabyte = kilobyte * 1000
-  const gigabyte = megabyte * 1000
+  const kilobyte = byte * 1024
+  const megabyte = kilobyte * 1024
+  const gigabyte = megabyte * 1024
   if (this > gigabyte) return (this.valueOf() / gigabyte).toFixed(2) + ' GB'
   if (this > megabyte) return (this.valueOf() / megabyte).toFixed(2) + ' MB'
   if (this > kilobyte) return (this.valueOf() / kilobyte).toFixed(2) + ' KB'
