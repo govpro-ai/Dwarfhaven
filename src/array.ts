75e45a1d43b4271db6a0760e9673d0764bfcc235
Kurt Apple <kapplepi@gmail.com>
2025-06-26 23:54:25 -0700

    array sum function that makes me happy

--- src/array.ts
+++ src/array.ts
@@ -8,6 +8,12 @@ declare global {
     last: () => T
     /** Returns a random element from an array. */
     random: () => T
+    /** Returns the singular or plural form of the provided string(s) */
+    pluralize: (itemType: string, pluralType?: string) => string
+    /** Returns the quantity of items in the array followed by the singular or plural form of the provided string(s) */
+    pluralizeqty: (itemType: string, pluralType?: string) => string
+    /** Returns the sum of the values or the key-values if specified in the parameters. 0 if types cannot be added. */
+    sum: (K?: keyof T | ((item: T) => any)) => number
   }
 }
 
@@ -21,4 +27,25 @@ Array.prototype.last = function () {
 
 Array.prototype.random = function () {
   return this[Math.floor(Math.random() * this.length)]
+}
+
+Array.prototype.pluralize = function (itemType, pluralType) {
+  return this.length > 1 ? pluralType ?? itemType+'s' : itemType
+}
+
+Array.prototype.pluralizeqty = function (itemType, pluralType) {
+  return `${this.length} ${this.pluralize(itemType, pluralType)}`
+}
+
+Array.prototype.sum = function<T>(K?: keyof T | ((item: T) => any)) {
+  if (K) {
+    if (typeof K === 'function') {
+      return this.map(K).sum()
+    }
+    this.map(x => x[K]).sum()
+  }
+  if(this.some(x => {
+    typeof x === 'undefined' || typeof x === 'symbol'
+  })) return 0
+  return this.reduce((acc, item) => acc + item, 0);
 }
